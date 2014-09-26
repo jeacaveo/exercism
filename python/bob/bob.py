f2509ef27ecad87b33163de9f27e7e7790e28c27
Jean Ventura <jv@venturasystems.net>
2014-09-26 18:35:45 -0400

    Second iteration for python/bob exercise.

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,43 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-from string import whitespace
 
 
-def hey(s):
-    def is_silent(s):
-        """ Silence s an empty or space/whitespce filled string. """
-        return True if not s or s.replace(' ', '') in whitespace else False
+def hey(message):
+    """ Conversation module for Bob's A.I system. """
 
-    def is_question(s):
+    def _is_silent(message):
+        """ Silence is an empty or space/whitespace filled string. """
+        return message.strip() == ''
+
+    def _is_question(message):
         """ A question always ends with a '?'. """
-        return True if s[-1] == '?' else False
+        return message.endswith('?')
 
-    def is_shout(s):
-        """
-        A shout is always in ALL CAPS.
-        '!' at the end is not shouting.
+    def _is_shout(message):
+        """ A shout is always in ALL CAPS. """
+        return message.isupper()
 
-        """
-        r = False
-        for char in s:
-            # Simultaneous validation of lower and upper cases.
-            # A single lower case letter makes it a non shout.
-            # If no letters (punctuation or numbers only), is not a shout.
-            if char.islower():
-                r = False
-                break
-            elif not r and char.isupper():
-                r = True
-        return r
-
-    if is_silent(s):
-        r = 'Fine. Be that way!'
-    elif is_shout(s):
-        r = 'Whoa, chill out!'
-    elif is_question(s):
-        r = 'Sure.'
-    else:
-        r = 'Whatever.'
-
-    return r
+    if _is_silent(message):
+        return 'Fine. Be that way!'
+    elif _is_shout(message):
+        return 'Whoa, chill out!'
+    elif _is_question(message):
+        return 'Sure.'
+    return 'Whatever.'
